2bd5c8468636482612b47286eb0cddd4a7ea5ff4
Andrepaolo <andrepaol32@gmail.com>
2022-07-17 20:11:44 -0500

    apla

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('¡Bienvenido a ACOMITA!') }}
         </h2>
     </x-slot>
 
